49239eee8c9e6c080d4c8d2988a513c8a528fca9
Arturo Hernandez <aihernandez14@miners.utep.edu>
2022-05-08 02:11:00 -0600

    Added test and buzzer sounds

--- Code/stateMachine.c
+++ Code/stateMachine.c
@@ -8,18 +8,41 @@
 
 char interruptTime;
 char button_four;
+u_int backgroundColor = COLOR_WHITE;
 
 void state_advance(){
-  
+
+  const u_char text_row = 100;
+  const u_char text_col = 40;
   my_shape(COLOR_RED);
 
   switch(state){
 
   default: state++;
-  case 1: my_color=COLOR_BLUE; move_shape_Left(10); break;
-  case 2: my_color=COLOR_RED; move_shape_Up(10); break;
-  case 3: my_color=COLOR_YELLOW; move_shape_Down(10); break;
-  case 4: my_color=COLOR_MAGENTA; move_shape_Right(10); break;
+  case 1:
+    my_color=COLOR_BLUE;
+    move_shape_Left(10);
+    buzzer_set_period(0);
+    drawString5x7(text_col, text_row, "Blue", COLOR_BLUE, backgroundColor);
+    break;
+  case 2:
+    my_color=COLOR_RED;
+    move_shape_Up(10);
+    buzzer_set_period(200);
+    drawString5x7(text_col, text_row, "Red", COLOR_RED, backgroundColor);
+    break;
+  case 3:
+    my_color=COLOR_YELLOW;
+    move_shape_Down(10);
+    buzzer_set_period(300);
+    drawString5x7(text_col, text_row, "Yellow", COLOR_YELLOW, backgroundColor);
+    break;
+  case 4:
+    my_color=COLOR_MAGENTA;
+    move_shape_Right(10);
+    buzzer_set_period(400);
+    drawString5x7(text_col, text_row, "Magenta", COLOR_MAGENTA, backgroundColor);
+    break;
   }
 }
 
